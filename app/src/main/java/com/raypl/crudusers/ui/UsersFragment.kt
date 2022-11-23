a628c180c5cf9b2af5e253f75ad5cdbe84fdc3db
Ray Pasache <ruddy.pasache@gmail.com>
2022-11-23 18:28:54 +0100

    config navigation component using view binding

--- app/src/main/java/com/raypl/crudusers/ui/UsersFragment.kt
+++ app/src/main/java/com/raypl/crudusers/ui/UsersFragment.kt
@@ -1,4 +1,22 @@
 package com.raypl.crudusers.ui
 
-class UsersFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.raypl.crudusers.databinding.FragmentUserListBinding
+
+class UsersFragment: Fragment() {
+
+    var binding: FragmentUserListBinding? = null
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentUserListBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
 }
